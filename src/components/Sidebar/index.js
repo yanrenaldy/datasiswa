241fc94f78f4c22bb949411094f64f47dd05023c
Ryan Renaldy <yyanrenaldy@gmail.com>
2022-09-08 13:22:42 +0700

    3rd commit

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   ProSidebar,
   SidebarHeader,
@@ -9,41 +10,82 @@ import {
 } from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
 import { Routes, Route, Link } from "react-router-dom";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Image } from "react-bootstrap";
 import { Home, Test1, DataSiswa } from "../../pages";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faPeopleGroup,
+  faSchool,
+  faBook,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Sidebar() {
+  const [windowHeight, setwindowHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    window.addEventListener("resize", setwindowHeight(window.innerHeight));
+  }, [windowHeight]);
+
   return (
     <div>
-        <Row>
-          <Col lg="2" >
-            <ProSidebar>
-              <SidebarContent>
-                {
-                  <Menu iconShape="square">
+      <Row>
+        <Col
+          lg="2"
+          style={{
+            height: windowHeight,
+          }}
+        >
+          <ProSidebar>
+            <SidebarHeader>
+              <Row>
+                <Col xs={3} style={{paddingLeft:"25px", paddingTop: "10px", paddingBottom: "10px"}}>
+                  <Image src="logos.png" height={"50px"} />
+                </Col>
+                <Col style={{paddingTop:"20px"}}>
+                  <h5>YPGMI Antiokhia</h5>
+                </Col>
+              </Row>
+            </SidebarHeader>
+            <SidebarContent>
+              {
+                <Menu iconShape="square">
+                  <MenuItem icon={<FontAwesomeIcon icon={faHome} />}>
+                    Home
+                    <Link to="/" />
+                  </MenuItem>
+                  <SubMenu
+                    title="Data Siswa"
+                    icon={<FontAwesomeIcon icon={faPeopleGroup} />}
+                  >
                     <MenuItem>
-                      Home
-                      <Link to="/" />
+                      Daftar Siswa
+                      <Link to="/siswa" />{" "}
                     </MenuItem>
-                    <SubMenu title="Data Siswa">
-                      <MenuItem>Daftar Siswa<Link to="/siswa"/> </MenuItem>
-                      <MenuItem>Absensi<Link to="/test"/></MenuItem>
-                    </SubMenu>
-                    <MenuItem>Kelas</MenuItem>
-                    <MenuItem>Tahun Ajaran</MenuItem>
-                  </Menu>
-                }
-              </SidebarContent>
-            </ProSidebar>
-          </Col>
-          <Col>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/test" element={<Test1 />} />
-              <Route path="/siswa" element={<DataSiswa />} />
-            </Routes>
-          </Col>
-        </Row>
+                    <MenuItem>
+                      Absensi
+                      <Link to="/test" />
+                    </MenuItem>
+                  </SubMenu>
+                  <MenuItem icon={<FontAwesomeIcon icon={faSchool} />}>
+                    Kelas
+                  </MenuItem>
+                  <MenuItem icon={<FontAwesomeIcon icon={faBook} />}>
+                    Tahun Ajaran
+                  </MenuItem>
+                </Menu>
+              }
+            </SidebarContent>
+          </ProSidebar>
+        </Col>
+        <Col>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/test" element={<Test1 />} />
+            <Route path="/siswa" element={<DataSiswa />} />
+          </Routes>
+        </Col>
+      </Row>
     </div>
   );
 }
--- src/pages/DataSiswa/index.js
+++ src/pages/DataSiswa/index.js
@@ -8,8 +8,10 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 function DataSiswa() {
   const [tahunAjaran, settahunAjaran] = useState([]);
+  const [daftarKelas, setdaftarKelas] = useState([]);
   const [dataArray, setdataArray] = useState({
     ta: "",
+    kelas: "",
   });
 
   useEffect(() => {
@@ -25,6 +27,27 @@ function DataSiswa() {
     };
   }, []);
 
+  useEffect(() => {
+    let isMounted = true;
+    axios
+      .get(API_URL + "daftarkelas")
+      .then((response) => response.data)
+      .then((data) => {
+        if (isMounted) setdaftarKelas(data);
+      });
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setdataArray((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <div>
       <div className="navbar-atas">
@@ -33,17 +56,31 @@ function DataSiswa() {
             <FontAwesomeIcon icon={faArrowLeft} className="panah" />
           </Col>
           <Col>
-            <Form.Select aria-label="Default select example" name="ta" required>
+            <Form.Select
+              aria-label="Default select example"
+              name="ta"
+              value={dataArray.ta}
+              onChange={onChange}
+              required
+            >
               <option>Tahun Ajaran</option>
+              {tahunAjaran.map((tahunAjaran, index) => (
+                <option key={index}>{tahunAjaran.Tables_in_siswaMetho}</option>
+              ))}
             </Form.Select>
           </Col>
           <Col>
             <Form.Select
               aria-label="Default select example"
               name="kelas"
+              value={dataArray.kelas}
+              onChange={onChange}
               required
             >
               <option>Kelas</option>
+              {daftarKelas.map((daftarKelas, index) => (
+                <option key={index}>{daftarKelas.namaKelas}</option>
+              ))}
             </Form.Select>
           </Col>
           <Col xs={1}>
